4587671a8b5fb55291128555c6c8ccea082b9147
Nvt-Test-NL <saucisse-lijn8y@icloud.com>
2025-09-21 22:03:40 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,12 @@ export default function RootLayout({
   <link rel="preconnect" href="https://image.tmdb.org" />
   <link rel="preconnect" href="https://images.ctfassets.net" />
   <link rel="preconnect" href="https://ssl.gstatic.com" />
+  <meta name="google-adsense-account" content="ca-pub-4865118146627791" />
+  <script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4865118146627791"
+    crossOrigin="anonymous"
+  />
   <script async src="https://cloud.umami.is/script.js" data-website-id="1f0a416d-67e8-435f-b6ec-9af2a2fa359f"></script>
       </head>
       <body className={`${inter.className} bg-[rgb(var(--background))] text-[rgb(var(--foreground))]`}>
@@ -41,3 +47,4 @@ export default function RootLayout({
     </html>
   )
 }
+
